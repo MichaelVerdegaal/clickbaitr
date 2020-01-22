1126e42329c94338d47ba11197312e0d0a35b1f9
Michael <michaelverdegaal@hotmail.nl>
2020-01-22 22:47:37 +0100

    implement writing function in scraping script

--- title_collection.py
+++ title_collection.py
@@ -1,6 +1,7 @@
 from bs4 import BeautifulSoup
 from string_util import fix_title
 import requests
+import csv
 
 
 def search_youtube(query):
@@ -12,14 +13,24 @@ def search_youtube(query):
         response = requests.get(url)
         soup = BeautifulSoup(response.content, "html.parser")
 
-        title_list = {query: []}
+        title_list = []
         for tile in soup.find_all(attrs={"class": "yt-lockup-tile"}):
             yt_uix_tile = tile.find(attrs={"class": "yt-uix-tile-link"})
             title = yt_uix_tile["title"]
             title = fix_title(title)
-            title_list[query].append(title)
+            title_list.append(title)
         return title_list
 
     except Exception as e:
         print(f"error: {e}")
         exit()
+
+
+with open("data.csv", "w", encoding="UTF-8") as file:
+    csv_writer = csv.writer(file, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
+    dictionary = open("words.txt")
+    for word in dictionary:
+        word = word.strip()
+        titles = search_youtube(word)
+        for title in titles:
+            csv_writer.writerow([word, title])

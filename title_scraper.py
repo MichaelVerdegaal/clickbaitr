63718f894322668f5a79a1a92dcf596ffeefe45b
Michael Verdegaal <michaelverdegaal@hotmail.nl>
2020-01-25 21:48:28 +0100

    merge files

--- title_scraper.py
+++ title_scraper.py
@@ -1,7 +1,23 @@
-from bs4 import BeautifulSoup
-from string_util import fix_title
-import requests
 import csv
+import re
+
+import requests
+from bs4 import BeautifulSoup
+
+
+def fix_title(title):
+    title = remove_punct(title)
+    title = remove_multiple_spaces(title)
+    return title
+
+
+def remove_punct(string):
+    string = re.sub(r"[']+", '', string)
+    return re.sub(r"[-:_!,/\[\].()#?;&\n]+", ' ', string).strip()
+
+
+def remove_multiple_spaces(string):
+    return re.sub(r'\s+', ' ', string).strip()
 
 
 def search_youtube(query):
